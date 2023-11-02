27c8351c72e823010556665837c5d13bd17e98e2
Clément Naves <clement.naves@telecom-sudparis.eu>
2023-11-02 11:39:54 +0100

    Ajout de la CLI (à compléter)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 extern crate zstd_decompressor;
+use std::string;
+
 use color_eyre::{self, eyre};
 use zstd_decompressor::parsing::ForwardByteParser;
+use clap::{Parser, command, arg};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    ///Dump information about frames instead of outputing the result
+    #[arg(short, long)]
+    info: String,
+}
 
 fn main() -> eyre::Result<()> {
     color_eyre::install()?;
+
+    let args = Args::parse();
+
+    
+    let file = std::fs::read(args.info);
+    
+    
 
     let mut parser = ForwardByteParser::new(&[0x12, 0x23, 0x34]);
     assert_eq!(0x12, parser.u8().unwrap());
