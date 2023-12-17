d8dcc74fcab51f8cd5ab7f7263ccd0fceb8c2bbc
AchilleBailly <achille.bailly@ensta-paris.fr>
2023-12-17 10:03:07 +0000

    updated alternating decoder

--- zstd-decompressor/src/decoders/alternating.rs
+++ zstd-decompressor/src/decoders/alternating.rs
@@ -1,82 +1,72 @@
-
-
-use super::{BitDecoder, fse::{FseDecoder, FseTable, self}};
-
-use std::{
-    fmt::{Debug, Display},
-    
+use super::{
+    fse::{self, FseDecoder, FseTable},
+    BitDecoder,
 };
 
+use std::fmt::Debug;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error{"Error {0} from the FseTable"}]
-    FseError(#[from] fse::Error)
+    FseError(#[from] fse::Error),
 }
-
-
 
 pub struct AlternatingDecoder {
-    first_decoder : FseDecoder,
-    second_decoder : FseDecoder,
-    last_updated_is_first : bool
+    first_decoder: FseDecoder,
+    second_decoder: FseDecoder,
+    last_updated_is_first: bool,
 }
 
-
 impl AlternatingDecoder {
-
-    fn new(table : FseTable) -> Self {
+    fn new(table: FseTable) -> Self {
         let bis_table = table.clone();
         AlternatingDecoder {
             first_decoder: FseDecoder::from(table),
             second_decoder: FseDecoder::from(bis_table),
             last_updated_is_first: false,
         }
-
     }
-
 }
 
-
-impl <'a>BitDecoder<'a, Error, u16> for AlternatingDecoder {
-    fn initialize(&mut self, bitstream: &mut impl crate::parsing::BitParser<'a>) -> Result<(), Error> {
-        self.first_decoder.initialize(bitstream);
+impl<'a> BitDecoder<'a, Error, u16> for AlternatingDecoder {
+    fn initialize(
+        &mut self,
+        bitstream: &mut impl crate::parsing::BitParser<'a>,
+    ) -> Result<(), Error> {
+        self.first_decoder.initialize(bitstream)?;
         self.last_updated_is_first = true;
-        self.second_decoder.initialize(bitstream);
-        self.last_updated_is_first= false;
+        self.second_decoder.initialize(bitstream)?;
+        self.last_updated_is_first = false;
         Ok(())
     }
 
     fn expected_bits(&self) -> usize {
         if self.last_updated_is_first {
             return self.second_decoder.expected_bits();
-        }
-        else {
+        } else {
             return self.first_decoder.expected_bits();
         }
     }
 
     fn symbol(&mut self) -> u16 {
         if self.last_updated_is_first {
-            self.last_updated_is_first = false;
-            return self.second_decoder.symbol(); 
-        }
-        else {
-            self.last_updated_is_first = true;
+            return self.second_decoder.symbol();
+        } else {
             return self.first_decoder.symbol();
         }
     }
 
-    fn update_bits(&mut self, bitstream: &mut impl crate::parsing::BitParser<'a>) -> Result<bool, Error> {
+    fn update_bits(
+        &mut self,
+        bitstream: &mut impl crate::parsing::BitParser<'a>,
+    ) -> Result<bool, Error> {
         if self.last_updated_is_first {
             self.last_updated_is_first = false;
+            return Ok(self.second_decoder.update_bits(bitstream)?);
+        } else {
+            self.last_updated_is_first = true;
             return Ok(self.first_decoder.update_bits(bitstream)?);
         }
-        else {
-            self.last_updated_is_first = true;
-            return Ok(self.second_decoder.update_bits(bitstream)?);
-        }
-        
     }
 
     fn reset(&mut self) {
